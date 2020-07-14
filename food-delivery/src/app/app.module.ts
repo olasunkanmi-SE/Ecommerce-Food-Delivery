8a13ff2057a0661074ac98ec9a4eb120b5422b3c
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-07-14 20:21:43 +0800

    create the navbar

--- food-delivery/src/app/app.module.ts
+++ food-delivery/src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { MatModule } from './shared/components/mat/mat.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './shared/components/nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
