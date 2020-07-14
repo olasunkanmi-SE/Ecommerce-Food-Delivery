7e5ed2f2c806cb91fe887c3b8958a0e150c4bd7c
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-07-15 00:36:41 +0800

    create the landing page with search

--- food-delivery/src/app/app.module.ts
+++ food-delivery/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './shared/components/nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
