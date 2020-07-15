87b96e565483f3c34af10c4f209c18c48efaf3a6
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-07-15 20:09:44 +0800

    implement geo location service to determine user address

--- food-delivery/src/app/app.module.ts
+++ food-delivery/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { MatModule } from './shared/components/mat/mat.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './shared/components/nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent],
@@ -14,6 +17,9 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({ apiKey: environment.googleAPIKey }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- food-delivery/src/environments/environment.ts
+++ food-delivery/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  googleAPIKey: 'AIzaSyA4T8IkToFAaIZzB7Y8Y59frBGMZu_GtpU',
 };
 
 /*
